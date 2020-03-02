3ea35d1b8be08bb09091b765f67072b6d73e51e8
JonathanJouffroy <jonathanjouffroy@gmail.com>
2020-03-02 11:00:58 +0100

    premier commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component {
+
+
+constructor(props) {
+  super(props)
+  this.state={
+  coronavirus:['Adbdel','Jérome']
+   }
+}
+
+handleChange = (e) => {
+  let name = e.target.name
+  this.setState({
+    [name]: e.target.value
+  })
+}
+
+addInfected = (e) =>{
+  this.setState({
+    coronavirus: [...this.state.coronavirus, this.state.infected]
+  })
+}
+
+componentDidMount = () =>{
+  console.log('component did mount')
+}
+
+  render() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+          {this.state.coronavirus.map((e,i) => (<p key={i}>{e}</p>))}
+        <input type='text' name='infected' onChange={(e) => this.handleChange(e)}/>
+        <button onClick={() => this.addInfected()}>Ajouter</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+}
+export default App;
