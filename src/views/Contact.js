af3d3312a73d797172e6c14f5f8bbe45c1f97134
Padrum69 <theo.vignon@ecole-isitech.fr>
2020-03-06 09:49:47 +0100

    None

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -7,120 +7,119 @@ class Contact extends React.Component {
 
   render() {
 
-  return (
-    <div>
-      <NavBar></NavBar>
-      <section className="contact-section my-5">
-      <MDBRow>
-        <MDBCol lg="8">
-          <MDBCardBody className="form">
-            <h3 className="mt-4">
-              <MDBIcon icon="envelope" className="pr-2" />
-             Nous contacter:
-            </h3>
-            <MDBRow>
-              <MDBCol md="6">
-                <div className="md-form mb-0">
-                  <MDBInput
-                    type="text"
-                    id="form-contact-name"
-                    label="Votre nom"
-                  />
-                </div>
-              </MDBCol>
-              <MDBCol md="6">
-                <div className="md-form mb-0">
-                  <MDBInput
-                    type="text"
-                    id="form-contact-email"
-                    label="Votre e-mail"
-                  />
-                </div>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol md="6">
-                <div className="md-form mb-0">
-                  <MDBInput
-                    type="text"
-                    id="form-contact-phone"
-                    label="Votre téléphone"
-                  />
-                </div>
-              </MDBCol>
-              <MDBCol md="6">
-                <div className="md-form mb-0">
-                  <MDBInput
-                    type="text"
-                    id="form-contact-company"
-                    label="Votre société"
-                  />
-                </div>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol md="12">
-                <div className="md-form mb-0">
-                  <MDBInput
-                    type="textarea"
-                    id="form-contact-message"
-                    label="Votre message"
-                  />
-                  <MDBBtn rounded color="blue">
-                    <MDBIcon icon="paper-plane" />
-                  </MDBBtn>
-                </div>
-              </MDBCol>
-            </MDBRow>
-          </MDBCardBody>
-        </MDBCol>
-        <MDBCol lg="4">
-          <MDBCardBody className="contact text-center h-100">
-            <h3 className="my-4 pb-2">Information de contact</h3>
-            <ul className="list-unstyled ml-4">
-              <li>
-                <p>
-                  <MDBIcon icon="map-marker-alt" className="pr-2" />
-                  Lyon, 69000 FRA
-                </p>
-              </li>
-              <li>
-                <p>
-                  <MDBIcon icon="phone" className="pr-2" />+ 01 234 567 89
-                </p>
-              </li>
-              <li>
-                <p>
-                  <MDBIcon icon="envelope" className="pr-2" />
-                  contact@dashboard.com
-                </p>
-              </li>
-            </ul>
-            <hr className="hr-light my-4" />
-            <ul className="list-inline text-center list-unstyled">
-              <li className="list-inline-item">
-                <a href="#!" className="p-2 fa-lg w-ic">
-                  <MDBIcon fab icon="twitter" />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#!" className="p-2 fa-lg w-ic">
-                  <MDBIcon fab icon="linkedin-in" />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#!" className="p-2 fa-lg w-ic">
-                  <MDBIcon fab icon="instagram" />
-                </a>
-              </li>
-            </ul>
-          </MDBCardBody>
-        </MDBCol>
-      </MDBRow>
-    </section>
-</div>
-
-  );
-}
+    return (
+      <div>
+        <NavBar></NavBar>
+        <section className="contact-section my-5">
+        <MDBRow>
+          <MDBCol lg="8">
+            <MDBCardBody className="form">
+              <h3 className="mt-4">
+                <MDBIcon icon="envelope" className="pr-2" />
+              Nous contacter:
+              </h3>
+              <MDBRow>
+                <MDBCol md="6">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="text"
+                      id="form-contact-name"
+                      label="Votre nom"
+                    />
+                  </div>
+                </MDBCol>
+                <MDBCol md="6">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="text"
+                      id="form-contact-email"
+                      label="Votre e-mail"
+                    />
+                  </div>
+                </MDBCol>
+              </MDBRow>
+              <MDBRow>
+                <MDBCol md="6">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="text"
+                      id="form-contact-phone"
+                      label="Votre téléphone"
+                    />
+                  </div>
+                </MDBCol>
+                <MDBCol md="6">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="text"
+                      id="form-contact-company"
+                      label="Votre société"
+                    />
+                  </div>
+                </MDBCol>
+              </MDBRow>
+              <MDBRow>
+                <MDBCol md="12">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="textarea"
+                      id="form-contact-message"
+                      label="Votre message"
+                    />
+                    <MDBBtn rounded color="blue">
+                      <MDBIcon icon="paper-plane" />
+                    </MDBBtn>
+                  </div>
+                </MDBCol>
+              </MDBRow>
+            </MDBCardBody>
+          </MDBCol>
+          <MDBCol lg="4">
+            <MDBCardBody className="contact text-center h-100">
+              <h3 className="my-4 pb-2">Information de contact</h3>
+              <ul className="list-unstyled ml-4">
+                <li>
+                  <p>
+                    <MDBIcon icon="map-marker-alt" className="pr-2" />
+                    Lyon, 69000 FRA
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <MDBIcon icon="phone" className="pr-2" />+ 01 234 567 89
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <MDBIcon icon="envelope" className="pr-2" />
+                    contact@dashboard.com
+                  </p>
+                </li>
+              </ul>
+              <hr className="hr-light my-4" />
+              <ul className="list-inline text-center list-unstyled">
+                <li className="list-inline-item">
+                  <a href="#!" className="p-2 fa-lg w-ic">
+                    <MDBIcon fab icon="twitter" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#!" className="p-2 fa-lg w-ic">
+                    <MDBIcon fab icon="linkedin-in" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#!" className="p-2 fa-lg w-ic">
+                    <MDBIcon fab icon="instagram" />
+                  </a>
+                </li>
+              </ul>
+            </MDBCardBody>
+          </MDBCol>
+        </MDBRow>
+      </section>
+    </div>
+    );
+  }
 }
 export default Contact;
