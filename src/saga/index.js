23df404937d18e4757e23702456fb16296af2659
JonathanJouffroy <jonathanjouffroy@gmail.com>
2020-04-03 11:18:02 +0200

    saga

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,12 +1,12 @@
 import { fork } from 'redux-saga/effects'
 import users from './users'
-import socket from './socket'
+//import socket from './socket'
 import products from './products'
 import orders from './orders'
 
 function* rootSaga(){
     yield fork(users)
-    yield fork(socket)
+    //yield fork(socket)
     yield fork(products)
     yield fork(orders)
 }
